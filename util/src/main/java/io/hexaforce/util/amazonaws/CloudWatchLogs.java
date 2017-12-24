d01c8a535f120d867f7e2c13478a289d6a175b5b
T.Tantaka <relics9@gmail.com>
2017-12-24 21:57:26 +0900

    kk

--- util/src/main/java/io/hexaforce/util/amazonaws/CloudWatchLogs.java
+++ util/src/main/java/io/hexaforce/util/amazonaws/CloudWatchLogs.java
@@ -1,5 +1,33 @@
 package io.hexaforce.util.amazonaws;
 
-public class CloudWatchLogs {
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
+import com.amazonaws.services.cloudwatch.model.ListMetricsRequest;
+import com.amazonaws.services.cloudwatch.model.ListMetricsResult;
+import com.amazonaws.services.cloudwatch.model.Metric;
 
+public class CloudWatchLogs {
+	
+	void aaa() {
+		final AmazonCloudWatch cw = AmazonCloudWatchClientBuilder.defaultClient();
+
+		boolean done = false;
+
+		while (!done) {
+			ListMetricsRequest request = new ListMetricsRequest().withMetricName("name").withNamespace("namespace");
+
+			ListMetricsResult response = cw.listMetrics(request);
+
+			for (Metric metric : response.getMetrics()) {
+				System.out.printf("Retrieved metric %s", metric.getMetricName());
+			}
+
+			request.setNextToken(response.getNextToken());
+
+			if (response.getNextToken() == null) {
+				done = true;
+			}
+		}
+	}
+	
 }
--- util/src/main/java/io/hexaforce/util/amazonaws/ElasticComputeCloud.java
+++ util/src/main/java/io/hexaforce/util/amazonaws/ElasticComputeCloud.java
@@ -1,5 +1,20 @@
 package io.hexaforce.util.amazonaws;
 
-public class ElasticComputeCloud {
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.InstanceType;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
 
+public class ElasticComputeCloud {
+	void ggg() {
+		final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+
+		RunInstancesRequest run_request = new RunInstancesRequest().withImageId(null)
+				.withInstanceType(InstanceType.T1Micro).withMaxCount(1).withMinCount(1);
+
+		RunInstancesResult run_response = ec2.runInstances(run_request);
+
+		String instance_id = run_response.getReservation().getReservationId();
+	}
 }
